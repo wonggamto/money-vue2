8798beb90504738b354625d15b98e648272ea1f2
Tom <wonggamto@163.com>
2020-09-19 23:46:10 +0800

    创建3个页面 money、labels、statistics

---
+++ src/views/Statistics.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>Statistics</div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Statistics extends Vue {
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
